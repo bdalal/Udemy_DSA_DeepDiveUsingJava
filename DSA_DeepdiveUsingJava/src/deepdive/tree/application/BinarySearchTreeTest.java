1015cd64819d95a4c225b207f3b20a94c186729d
binoy.dalal <binoy.dalal@lucidworks.com>
2018-08-18 17:53:27 -0500

    updated tests

--- DSA_DeepdiveUsingJava/src/deepdive/tree/application/BinarySearchTreeTest.java
+++ DSA_DeepdiveUsingJava/src/deepdive/tree/application/BinarySearchTreeTest.java
@@ -7,28 +7,30 @@ import java.util.Comparator;
 
 public class BinarySearchTreeTest {
     public static void main(String args[]) throws IllegalAccessException {
-        BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>(25);
-        Comparator<Integer> c = new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1.intValue() - o2.intValue();
-            }
-        };
-        bst.insert(20, c);
-        bst.insert(15, c);
-        bst.insert(27, c);
-        bst.insert(30, c);
-        bst.insert(29, c);
-        bst.insert(26, c);
-        bst.insert(22, c);
-        bst.insert(32, c);
+        Comparator<Integer> c = (o1, o2) -> o1 - o2;
+        BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>(25, c);
+        bst.insert(20);
+        bst.insert(15);
+        bst.insert(27);
+        bst.insert(30);
+        bst.insert(29);
+        bst.insert(26);
+        bst.insert(22);
+        bst.insert(32);
+        bst.insert(17);
+        bst.insert(33);
+        bst.insert(16);
 
-        System.out.println(bst.traverse("BFS"));
+        System.out.println("BFS : " + bst.traverse("BFS"));
+        System.out.println("LNR : " + bst.traverse("LNR"));
+        System.out.println("NLR : " + bst.traverse("NLR"));
+        System.out.println("LRN : " + bst.traverse("LRN"));
 
         System.out.println(bst.min());
         System.out.println(bst.max());
 
-        TreeNode<Integer> got = bst.get(29, c);
+        TreeNode<Integer> got = bst.get(33);
         System.out.println(got != null ? ((TreeNode) got).getData() : "NULL");
+        System.out.println("Parent : " + got.getParent().getData());
     }
 }
